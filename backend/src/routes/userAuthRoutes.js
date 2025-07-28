92bd5f82f234c17bb5634df1df485f3ec05af47e
mihir3030 <mihirdholakia777@gmail.com>
2025-07-28 15:51:56 +0530

    Signup controller added

---
+++ backend/src/routes/userAuthRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import signup from '../controllers/authControllers.js'
+
+// createrouter
+const router = express.Router()
+
+// create routes for User Authentication
+// router.get("/signup", (req, res) => {
+//     res.send("signupp done")
+// })
+
+//signUp Route
+router.post("/signup", signup)
+
+export default router
