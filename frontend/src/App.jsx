ed416a593fc070ecb2cf6f72103031f95bdeee08
mihir3030 <mihirdholakia777@gmail.com>
2025-07-28 09:38:40 +0530

    Navbar and sidear fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import Navbar from "./components/Navbar"
-import {Route, Routes} from "react-router-dom"
+import {Route, Routes, useLocation} from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import DashboardPage from "./pages/DashboardPage"
+import Sidebar from "./components/Dashboard/Sidebar/Sidebar"
 
 function App() {
+  const location = useLocation()
+
+  // define side bar and navbar show
+  const showNavbar = ["/", "/contact"].includes(location.pathname)
+  // const showSidebar = location.pathname.startsWith("/dashboard")
   return (
     <div>
-      <Navbar />
+      {showNavbar && <Navbar />}
+      {/* {showSidebar && <Sidebar />} */}
+  
       <Routes>
+        {/* Navbar routes */}
         <Route path="/" element={<HomePage />} />
+       
+       {/* sidebar routes */}
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </div>
   )
